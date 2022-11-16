9df74c74e7b9a8996f03819e0fc3d5b7c167780d
Daniel de Luna Rodríguez <danidlr02@gmail.com>
2022-11-16 14:44:44 +0100

    Arreglo cors

--- airlines-tickets/src/test/java/com/example/airlinestickets/TravelServiceTest.java
+++ airlines-tickets/src/test/java/com/example/airlinestickets/TravelServiceTest.java
@@ -76,7 +76,7 @@ public class TravelServiceTest {
             travelService.getDestinations(destinos4);
         });
     }
-    
+
     /*
     @Test
     void whenQueryOfDayIsDone_ReturnTravelsWithThreedaysOfDifference(){
